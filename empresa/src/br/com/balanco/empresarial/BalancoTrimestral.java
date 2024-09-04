5cb24c32279f599a43f78d98cd6ba7a79b13e9d6
leticiapzs <leticia_95lima@hotmail.com>
2024-09-04 16:01:11 -0300

    Balanço Trimestral

---
+++ empresa/src/br/com/balanco/empresarial/BalancoTrimestral.java
@@ -0,0 +1,26 @@
+package br.com.balanco.empresarial;
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+		
+		//Declarando a variável de gastos dos meses de Janeiro, Fevereiro e Março
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		
+		//Calculo do gasto trimestral
+		int gastosTrimestre = (gastosJaneiro + gastosFevereiro + gastosMarco);
+		
+		//Exibe os gatos do Trimestre
+		System.out.println(gastosTrimestre);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
